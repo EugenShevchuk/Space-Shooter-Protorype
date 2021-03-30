bd4b5da8867cf70ef1b038e1904c850bc8dc3b83
EugenShevchuk <ujinshevchuk@gmail.com>
2021-03-30 23:09:50 +0300

    Project started!

---
+++ Assets/Scripts/Singletone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
